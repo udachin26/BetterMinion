c8a4ba07534465868cbf654d6791d78c9b3f0fd4
Khang Nguyen <77101686+Mcbeany@users.noreply.github.com>
2021-08-09 17:19:57 +0700

    no more SlayingMinion

--- src/Mcbeany/BetterMinion/minions/MinionType.php
+++ src/Mcbeany/BetterMinion/minions/MinionType.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace Mcbeany\BetterMinion\minions;
 
-use Mcbeany\BetterMinion\BetterMinion;
 use Mcbeany\BetterMinion\entities\objects\MinionTree;
 use pocketmine\block\Block;
-use pocketmine\entity\Entity;
-use pocketmine\level\Level;
-use pocketmine\math\Vector3;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\nbt\tag\IntTag;
 
@@ -19,8 +15,6 @@ class MinionType implements MinionNBT
     const MINING_MINION = 0;
     const FARMING_MINION = 1;
     const LUMBERJACK_MINION = 2;
-    const SLAYING_MINION = 3;
-    const FISHING_MINION = 4;
 
     /** @var int */
     private $actionType;
@@ -56,12 +50,6 @@ class MinionType implements MinionNBT
         return Block::get($this->getTargetId(), $this->getTargetMeta());
     }
 
-    public function toEntity(Level $level, Vector3 $pos): Entity
-    {
-        $class = $this->getTargetName();
-        return new $class($this->getTargetId(), $level, Entity::createBaseNBT($pos));
-    }
-
     public function toTree(): MinionTree
     {
         return new MinionTree($this->toBlock());
@@ -69,11 +57,6 @@ class MinionType implements MinionNBT
 
     public function getTargetName(): string
     {
-        if ($this->getActionType() === self::SLAYING_MINION) {
-            $class = new \ReflectionClass(Entity::class);
-            $entities = $class->getStaticPropertyValue("knownEntities");
-            return $entities[$this->getTargetId()];
-        }
         return $this->toBlock()->getName();
     }
 
