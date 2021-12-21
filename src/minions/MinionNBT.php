984524d630eac302cf8fed6715f5b55f8c26a687
NgLamIsSimp <nglamvn2911@gmail.com>
2021-12-21 07:13:25 +0700

    CS.

--- src/minions/MinionNBT.php
+++ src/minions/MinionNBT.php
@@ -8,28 +8,28 @@ use pocketmine\nbt\tag\CompoundTag;
 
 interface MinionNBT{
 
-    // Info
-    public const INFO = "minionInfo";
-    public const TYPE = "type";
-    public const TARGET = "target";
-    public const UPGRADE = "upgrade";
+	// Info
+	public const INFO = "minionInfo";
+	public const TYPE = "type";
+	public const TARGET = "target";
+	public const UPGRADE = "upgrade";
 	public const ACTION_TIME = "actionTime";
-    public const LEVEL = "level";
-    public const MONEY_HELD = "moneyHeld";
-    public const COLLECTED_RESOURCES = "collectedResources";
+	public const LEVEL = "level";
+	public const MONEY_HELD = "moneyHeld";
+	public const COLLECTED_RESOURCES = "collectedResources";
 
-    // Target
-    public const BLOCK_ID = "blockId";
-    public const VARIANT = "variant";
+	// Target
+	public const BLOCK_ID = "blockId";
+	public const VARIANT = "variant";
 
-    // Inv
-    public const INV = "minionInv";
+	// Inv
+	public const INV = "minionInv";
 
-    // Upgrade
-    public const AUTO_SMELTER = "autoSmelter";
-    public const AUTO_SELLER = "autoSeller";
-    public const COMPACTOR = "compactor";
-    public const EXPANDER = "expander";
+	// Upgrade
+	public const AUTO_SMELTER = "autoSmelter";
+	public const AUTO_SELLER = "autoSeller";
+	public const COMPACTOR = "compactor";
+	public const EXPANDER = "expander";
 
 	public static function nbtDeserialize(CompoundTag $nbt);
 
