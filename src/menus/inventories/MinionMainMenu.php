7a385a9ccb4232801dfe79a1dbb645ed79070340
Ngọc Lam <nglamvn2911@gmail.com>
2022-01-06 15:14:33 +0700

    Better GUI design.

--- src/menus/inventories/MinionMainMenu.php
+++ src/menus/inventories/MinionMainMenu.php
@@ -7,9 +7,13 @@ use Mcbeany\BetterMinion\BetterMinion;
 use Mcbeany\BetterMinion\entities\BaseMinion;
 use Mcbeany\BetterMinion\menus\InventoryMenu;
 use Mcbeany\BetterMinion\utils\Language;
+use Mcbeany\BetterMinion\utils\Utils;
 use muqsit\invmenu\type\InvMenuTypeIds;
+use pocketmine\block\BlockFactory;
+use pocketmine\block\BlockLegacyIds;
 use pocketmine\block\utils\DyeColor;
 use pocketmine\block\VanillaBlocks;
+use pocketmine\item\VanillaItems;
 use pocketmine\player\Player;
 
 class MinionMainMenu extends InventoryMenu{
@@ -32,6 +36,16 @@ class MinionMainMenu extends InventoryMenu{
 				VanillaBlocks::STAINED_GLASS()->setColor(DyeColor::RED())->asItem()->setCustomName("Unlock at level " . $i);
 			$inv->setItem((int) (21 + ($i % 5) + (9 * (floor($i / 5)))), $invItem);
 		}
+		$info_item = VanillaItems::PLAYER_HEAD()->setCustomName("§r§f" . $this->getMinion()->getOriginalNameTag());
+		$info_item->setLore([
+			"§r§fTier: " . Utils::getRomanNumeral($this->getMinion()->getMinionInfo()->getLevel()),
+			"§r§fCollected Resources: " . $this->getMinion()->getMinionInfo()->getCollectedResources(),
+		]);
+		$inv->setItem(4, $info_item);
+		$inv->setItem(10, VanillaBlocks::FURNACE()->asItem()->setCustomName("AutoSmelter")->setLore(["Comming soon !"]));
+		$inv->setItem(19, VanillaBlocks::HOPPER()->asItem()->setCustomName("AutoSell")->setLore(["Comming soon !"]));
+		$inv->setItem(28, VanillaBlocks::LEGACY_STONECUTTER()->asItem()->setCustomName("Compactor")->setLore(["Comming soon !"]));
+		$inv->setItem(37, BlockFactory::getInstance()->get(BlockLegacyIds::COMMAND_BLOCK, 0)->asItem()->setCustomName("Expander")->setLore(["Comming soon !"]));
 		$inv->setItem(48, VanillaBlocks::CHEST()->asItem()->setCustomName("Retrieve all results"));
 		$inv->setItem(53, VanillaBlocks::BEDROCK()->asItem()->setCustomName("Remove your minion"));
 	}
